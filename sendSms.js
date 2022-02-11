b1a44be39e5c414327d8eff6522d98ba2106ad6a
Natnael Tekletsadik <natnaeltekletsadik@Natnaels-MacBook-Air.local>
2022-02-10 21:57:20 -0500

    create server

--- sendSms.js
+++ sendSms.js
@@ -1,2 +1,15 @@
-const accountSid = 0;
-const authToken = 0;
+const accountSid = 'ACf200703aa2fd43f4ecf5f4178b2cb3a2';
+const authToken = '3b038fa02ce2ecbb83816339c3df3f4c';
+
+const client = require('twilio')(accountSid, authToken);
+
+client.messages.create({
+  to: '+16479049791',
+  from: '+14377478266',
+  body: 'well i dont know what to say so...'
+})
+.then((msg) =>{
+  console.log(msg);
+}).catch(err => {
+  console.log(err);
+})
---
+++ server.js
@@ -0,0 +1,8 @@
+const exresss = require('express');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+const app = express();
+
+app.post('/sms' , (req, res) =>{
+  
+})
